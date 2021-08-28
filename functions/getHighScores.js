8f9b325f49fd72a366ffc641c4aaeef11237ae6c
masterdrop <joa193@hotmail.com>
2021-08-27 22:18:51 -0300

    add saveHighScore function and some refactoring

--- functions/getHighScores.js
+++ functions/getHighScores.js
@@ -1,25 +1,11 @@
-require('dotenv').config();
-const Airtable = require("airtable")
-
-Airtable.configure({
-    apiKey: process.env.AIRTABLE_API_KEY
-})
-
-const base = Airtable.base(process.env.AIRTABLE_BASE)
-const table = base(process.env.AIRTABLE_TABLE)
+const {table, getHighScores} = require("./utils/airtable")
 
 exports.handler = async (event) => {
     try{
-        const records = await table
-            .select({sort:[{field: 'score', direction: 'desc'}],filterByFormula: `AND(name != "",score > 0)`})
-            .firstPage()
-        const formattedRecords = records.map(record => ({
-            id: record.id,
-            fields: record.fields
-        }))
+        const records = await getHighScores(true)
         return {
             statusCode: 200,
-            body: JSON.stringify(formattedRecords)
+            body: JSON.stringify(records)
         }
     }catch(error){
         return {
---
+++ functions/saveHighScore.js
@@ -0,0 +1,49 @@
+const {table, getHighScores} = require("./utils/airtable")
+
+exports.handler = async (event) => {
+    if(event.httpMethod !== "POST"){
+        return {
+            statusCode: 405,
+            body: JSON.stringify({error:"Method not allowed"})
+        }
+    }
+    const {score,name} = JSON.parse(event.body)
+    if(!score || !name){
+        return {
+            statusCode:400,
+            body: JSON.stringify({error:"Bad request"})
+        }
+    }
+    //console.log(body)
+    try{
+        const records = await getHighScores(false)
+
+        const lowestRecord = records[9]
+        //console.log(lowestRecord)
+        if(typeof lowestRecord.fields.score === "undefined" || score > lowestRecord.fields.score){
+            const updateRecord = {
+                id: lowestRecord.id,
+                fields: {name,score},
+            }
+            await table.update([updateRecord])
+            return {
+                statusCode: 200,
+                body: JSON.stringify(updateRecord)
+            }
+        }else{
+            return {
+                statusCode: 200,
+                body: JSON.stringify({})
+            }
+        }
+
+        
+    }catch(error){
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                err: "Failed to query record in Airtable"
+            })
+        }
+    }
+}
