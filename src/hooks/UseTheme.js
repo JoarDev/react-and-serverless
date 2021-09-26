3248caae516c4fe1e7091670e49c56a9402ca298
masterdrop <joa193@hotmail.com>
2021-09-26 12:34:39 -0300

    Added persistence in user theme preference

--- src/hooks/UseTheme.js
+++ src/hooks/UseTheme.js
@@ -1,13 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default () => {
     const [theme, setTheme] = useState("light")
+
+    useEffect(()=>{
+        const localStorageTheme = window.localStorage.getItem("theme")
+        setTheme(localStorageTheme || "light")
+    },[])
     
     const toggleTheme = () => {
         if (theme === "light"){
             setTheme("dark")
+            window.localStorage.setItem("theme","dark")
         }else{
             setTheme("light")
+            window.localStorage.setItem("theme","light")
         }
     }
 
